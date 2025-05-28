1437bc92b2289c7968c5a1924d376b42d27d5813
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-05-28 18:56:05 +0530

    express vali

--- day7api/routes/userRoutes.js
+++ day7api/routes/userRoutes.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
+const upload = require("../middleware/multer");
+const UserModel = require("../models/UserModel");
 
 const userRoutes = express.Router();
 
@@ -8,11 +11,42 @@ userRoutes.get("/", (req, res) => {
   });
 });
 
-userRoutes.post("/register", (req, res) => {
-  res.json({
-    message: "User registration successful",
-    userData: req.body.userName,
-  });
-});
+userRoutes.post(
+  "/add",
+  upload,
+  [
+    body("name").notEmpty(),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+    body("gender").notEmpty(),
+    body("hobby").isArray({ min: 1 }),
+    body("city").notEmpty(),
+  ],
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    const { name, email, password, gender, hobby, city } = req.body;
+    const file = req.file?.filename || "";
+    try {
+      const userData = new UserModel({
+        name,
+        email,
+        password,
+        gender,
+        hobby,
+        city,
+        file,
+      }); // create data in mongodb
+      await userData.save();
+      return res
+        .status(201)
+        .json({ success: true, message: "your data added " });
+    } catch (error) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+);
 
 module.exports = userRoutes;
